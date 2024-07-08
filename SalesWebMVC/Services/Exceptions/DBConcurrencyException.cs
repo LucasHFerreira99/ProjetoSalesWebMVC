59b210fe4bf5c0a4e42f7a7d01e6b9e1c98467e6
Lucas Ferreira <lucashf99@outlook.com.br>
2024-07-08 15:51:51 -0300

    Editar vendedor e excessões de serviço personalizadas

---
+++ SalesWebMVC/Services/Exceptions/DBConcurrencyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class DBConcurrencyException : ApplicationException
+    {
+        public DBConcurrencyException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -3,7 +3,7 @@ using SalesWebMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using SalesWebMVC.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Services
@@ -38,6 +38,23 @@ namespace SalesWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x=> x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }catch (DbUpdateConcurrencyException e)
+            {
+                throw new DBConcurrencyException(e.Message);
+            }
+
         }
     }
 }
