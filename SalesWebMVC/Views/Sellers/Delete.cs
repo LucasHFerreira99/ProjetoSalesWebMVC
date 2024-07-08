9683e097dee5aa8decfe744606c3d9195db605f0
Lucas Ferreira <lucashf99@outlook.com.br>
2024-07-08 11:20:23 -0300

    Criado o Delete de Seller

---
+++ SalesWebMVC/Views/Sellers/Delete.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Views.Sellers
+{
+    public class Delete
+    {
+    }
+}
